c660d9a2eedaa50d7193d9b63e93874f6259e9b4
David Rowe <david.t.rowe@slalom.com>
2017-11-16 14:41:37 -0600

    fix deploy

--- heroku-deploy.sh
+++ heroku-deploy.sh
@@ -1,5 +1,5 @@
 cd ../client; npm install; npm run build; cp -r build ../server/build; cd -;
 git add .
 git commit -am "Deploy to heroku"
-push -f heroku master
+git push -f heroku master
 git reset --hard origin/master
